5d2ca8bf04c6fa627b28ec1b493febb8b91e1d1a
cfy-github <cfycq2009@gmail.com>
2014-10-15 12:59:53 +0800

    setting goal and start points

--- Classes/TileMapScene.cpp
+++ Classes/TileMapScene.cpp
@@ -20,28 +20,171 @@ Scene* TileMapScene::createScene() {
     return scene;
 }
 
+Point TileMapScene::position2TileCoord(Point p) {
+    auto x=static_cast<int>(p.x/_tileMap->getTileSize().width);
+    auto y=static_cast<int>((_tileMap->getTileSize().height*_tileMap->getMapSize().height-p.y)/_tileMap->getTileSize().height);
+    return Point(x,y);
+}
+
+void TileMapScene::checkLongPress(float t) {
+    //CCLOG("In check\n");
+    this->unschedule(schedule_selector(TileMapScene::checkLongPress));
+    
+    if((isStartTouched || isGoalTouched) && !isTouchMoved) {
+        if(isStartTouched) {
+           // CCLOG("check long start\n");
+            _start->setScale(2.0f);
+            _start->setOpacity(200);
+            isStartLongPressed=true;
+        }
+        
+        if(isGoalTouched) {
+           // CCLOG("check long goal\n");
+            _goal->setScale(2.0f);
+            _goal->setOpacity(200);
+            isGoalLongPressed=true;
+        }
+    }
+    
+}
+
+bool TileMapScene::onTouchBegan(Touch* t, Event* e) {
+   // CCLOG("Touch Begin\n");
+    Point loc = t->getLocationInView();
+    loc=Director::getInstance()->convertToGL(loc);
+    loc=convertToNodeSpace(loc);
+    //CCLOG("%f, %f\n", loc.x, loc.y);
+    
+    auto bbox=_start->getBoundingBox();
+    //CCLOG("%f, %f, %f, %f\n", bbox.getMinX(), bbox.getMinY(),bbox.getMaxX(), bbox.getMaxY());
+    
+    if(_start->getBoundingBox().containsPoint(loc)) {
+        //Click the start point
+       // CCLOG("Start Touched\n");
+        isStartTouched=true;
+        this->schedule(schedule_selector(TileMapScene::checkLongPress), 0.8f);
+    }
+    
+    if(_goal->getBoundingBox().containsPoint(loc)) {
+        //Click the goal point
+        isGoalTouched=true;
+        this->schedule(schedule_selector(TileMapScene::checkLongPress), 1.2f);
+    }
+    
+    //Todo other
+    return true;
+}
+
+void TileMapScene::onTouchMoved(Touch* t, Event* e) {
+    //CCLOG("In Touch Moved\n");
+    Point deltaPoint=t->getDelta();
+    
+    auto tLoc=t->getLocationInView();
+    tLoc=Director::getInstance()->convertToGL(tLoc);
+    tLoc=convertToNodeSpace(tLoc);
+    
+    if(fabs(deltaPoint.x)>1 || fabs(deltaPoint.y)>1) {
+        isTouchMoved=true;
+        
+        //Dragging start sprite
+        if(isStartLongPressed) {
+           // CCLOG("Start Dragging\n");
+            auto originTileCoord=position2TileCoord(_tileMap->convertToNodeSpace(_start->getPosition()));
+            auto loc1=_tileMap->convertToNodeSpace(_start->getPosition());
+            auto loc2=_start->getPosition();
+            
+            auto currTileCoord=position2TileCoord(tLoc);
+            //CCLOG("%f, %f,,,%f, %f----%f,%f\n", loc1.x, loc1.y, loc2.x, loc2.y, tLoc.x, tLoc.y);
+            if(originTileCoord!=currTileCoord) {
+                //Move start sprite;
+                _start->setPosition(tileCoord2Center(currTileCoord));
+            }
+        }
+        
+        //Dragging goal sprite
+        if(isGoalLongPressed) {
+            auto originTileCoord=position2TileCoord(_tileMap->convertToNodeSpace(_goal->getPosition()));
+            auto currTileCoord=position2TileCoord(tLoc);
+            if(originTileCoord!=currTileCoord) {
+                //Move goal sprite;
+                //CCLOG("To another tile\n");
+                _goal->setPosition(tileCoord2Center(currTileCoord));
+            }
+        }
+        
+    }
+    
+    
+    //TODO
+}
+
+void TileMapScene::onTouchCancelled(Touch* t, Event* e) {
+    if(isStartTouched) {
+        isStartTouched=false;
+        if(isStartLongPressed){
+            isStartLongPressed=false;
+            _start->setScale(1.0f);
+            _start->setOpacity(255);
+        }
+    }
+    
+    if(isGoalTouched) {
+        isGoalTouched=false;
+        if(isGoalLongPressed){
+            isGoalLongPressed=false;
+            _goal->setScale(1.0f);
+            _goal->setOpacity(255);
+        }
+    }
+    
+    //TODO
+    isTouchMoved=false;
+}
+
+void TileMapScene::onTouchEnded(Touch* t, Event* e) {
+    if(isStartTouched) {
+        isStartTouched=false;
+        if(isStartLongPressed){
+            isStartLongPressed=false;
+            _start->setScale(1.0f);
+            _start->setOpacity(255);
+        }
+    }
+    
+    if(isGoalTouched) {
+        isGoalTouched=false;
+        if(isGoalLongPressed){
+            isGoalLongPressed=false;
+            _goal->setScale(1.0f);
+            _goal->setOpacity(255);
+        }
+    }
+    
+    //TODO
+    isTouchMoved=false;
+}
+
 void TileMapScene::initTouchEvent() {
     auto listener=EventListenerTouchOneByOne::create();
     
     listener->setSwallowTouches(true);
     
-    listener->onTouchBegan = [](Touch *t, Event* e) {
-        auto target = static_cast<Sprite*>(e->getCurrentTarget());
-        
-        //Get the position of the current point relative to the button
-        Point locationInNode = target->convertToNodeSpace(t->getLocation());
-        Size s = target->getContentSize();
-        Rect rect = Rect(0, 0, s.width, s.height);
-        
-        //TODO
-        return true;
-    };
+    listener->onTouchBegan = CC_CALLBACK_2(TileMapScene::onTouchBegan, this);
     
-    //TODO
-    listener->onTouchMoved = [](Touch* t, Event* e) {};
-    listener->onTouchEnded = [](Touch* t, Event* e) {};
+    listener->onTouchMoved = CC_CALLBACK_2(TileMapScene::onTouchMoved, this);
     
-    _eventDispatcher->addEventListenerWithSceneGraphPriority(listener, this);//This?
+    listener->onTouchEnded = CC_CALLBACK_2(TileMapScene::onTouchEnded, this);
+    
+    listener->onTouchCancelled = CC_CALLBACK_2(TileMapScene::onTouchCancelled, this);
+    
+    
+    _eventDispatcher->addEventListenerWithSceneGraphPriority(listener, this);
+}
+
+void TileMapScene::initImageCache() {
+    _startTileTexture=Director::getInstance()->getTextureCache()->addImage("start.png");
+    _goalTileTexture=Director::getInstance()->getTextureCache()->addImage("goal.png");
+    _defaultTileTexture=Director::getInstance()->getTextureCache()->addImage("tile.png");
 }
 
 bool TileMapScene::init() {
@@ -49,11 +192,15 @@ bool TileMapScene::init() {
     // 1. super init first
     if ( !Layer::init() ) return false;
     
+    
+    initImageCache();
     //Load tile map
     // create a TMX map
     _tileMap=TMXTiledMap::create("pathFind.tmx");
-    addChild(_tileMap, 1, 66);
+    addChild(_tileMap, 0, 66);
     _background=_tileMap->getLayer("Background");
+    auto bckPos=_background->getPosition();
+    
     
     auto objGroup=_tileMap->getObjectGroup("objects");
     if(objGroup==NULL) {
@@ -61,18 +208,64 @@ bool TileMapScene::init() {
         return false;
     }
     
-    auto initPoint=objGroup->getObject("initPoint");
+    auto initObj=objGroup->getObject("initPoint");
+    auto goalObj=objGroup->getObject("goalPoint");
+    auto dftObj=objGroup->getObject("defaultPoint");
     
-    int x=initPoint["x"].asInt();
-    
-    int y=initPoint["y"].asInt();
-    
-    setViewPointCetner(Point(x,y));
+    auto initPoint=Point(initObj["x"].asInt(), initObj["y"].asInt());
+    auto goalPoint=Point(goalObj["x"].asInt(), initObj["y"].asInt());
+    auto dftPoint=Point(dftObj["x"].asInt(), initObj["y"].asInt());
     
     
-    //_meta=_tileMap->layerNamed("Meta");
+    auto initTileCoord=position2TileCoord(initPoint);
+    auto goalTileCoord=position2TileCoord(goalPoint);
+    auto dftTileCoord=position2TileCoord(dftPoint);
+    
+    //Init start sprite;
+    _start=Sprite::createWithTexture(_startTileTexture);
+    _start->setAnchorPoint(Vec2(0.5,0.5));
+    auto startCenter=tileCoord2Center(initTileCoord);
+    _start->setPosition(startCenter);
+    addChild(_start, 10, 70);
+    
+    //Init goal sprite
+    _goal=Sprite::createWithTexture(_goalTileTexture);
+    _goal->setAnchorPoint(Vec2(0.5,0.5));
+    auto goalCenter=tileCoord2Center(goalTileCoord);
+    _goal->setPosition(goalCenter);
+    addChild(_goal, 10, 80);
+    
+    initTouchEvent();
+    
+    setViewPointCetner(startCenter);
     
     return true;
+}
+
+Point TileMapScene::tileCoord2Center(Point coord) {
+    auto size=_tileMap->getTileSize();
+    auto mSize=_tileMap->getMapSize();
+    Point ret;
+    ret.x=(coord.x+0.5)*size.width;
+    ret.y=(mSize.height-coord.y-0.5)*size.height;
+    
+    return ret;
+}
+
+void TileMapScene::setAnchorPoint2Center(Sprite* sp) {
+    if(!sp) return;
+    sp->setAnchorPoint(Vec2(0.5, 0.5));
+    auto size=sp->getContentSize();
+    auto pos=sp->getPosition();
+    sp->setPosition(pos.x+0.5*size.width, pos.y+0.5*size.height);
+}
+
+void TileMapScene::setAnchorPoint2Origin(Sprite* sp) {
+    if(!sp) return;
+    sp->setAnchorPoint(Vec2::ZERO);
+    auto size=sp->getContentSize();
+    auto pos=sp->getPosition();
+    sp->setPosition(pos.x-0.5*size.width, pos.y-0.5*size.height);
 }
 
 void TileMapScene::setViewPointCetner(Point p) {
--- Classes/TileMapScene.h
+++ Classes/TileMapScene.h
@@ -23,24 +23,67 @@ public:
     
     void menuCloseCallback(cocos2d::Ref* pSender);
     
+    virtual bool onTouchBegan(Touch* t, Event* e);
+    virtual void onTouchMoved(Touch* t, Event* e);
+    virtual void onTouchEnded(Touch* t, Event* e);
+    virtual void onTouchCancelled(Touch* t, Event* e);
+    
     //Start callback
     //void menuStartCallback(cocos2d::Ref* pSender);
     
     // implement the "static create()" method manually
     CREATE_FUNC(TileMapScene);
+    
+    static inline long millisecondNow() {
+        struct timeval now;
+        gettimeofday(&now, NULL);
+        return (now.tv_sec * 1000 + now.tv_usec / 1000);
+    }
+    
 private:
-    Point tileCoordForPosition(Point pos);
+    //Point tileCoordForPosition(Point pos);
     
     void initTouchEvent();
     
     void setViewPointCetner(Point p);
     
+    Point position2TileCoord(Point pos);
+    
+    Point tileCoord2Center(Point p);
+    
+    void checkLongPress(float t);
+    
+    void setAnchorPoint2Center(Sprite* sp);
+    void setAnchorPoint2Origin(Sprite* sp);
+    
+    void initImageCache();
+    
+    //void touchMoved
+    
 private:
     TMXTiledMap* _tileMap;
     TMXLayer* _background;
-    Sprite* _player;
+    TMXLayer* _foreground;
+    //Sprite* _player;
+    
+    Sprite* _start;
+    Sprite* _goal;
+    
+    Texture2D* _defaultTileTexture;
+    Texture2D* _startTileTexture;
+    Texture2D* _goalTileTexture;
     
     TMXLayer* _meta;
+    
+    //Touch
+    bool isTouchMoved=false;
+    //bool isLongTouch=false;
+    bool isStartLongPressed=false;
+    bool isGoalLongPressed=false;
+    
+    bool isStartTouched=false;
+    bool isGoalTouched=false;
+    
 };
 
 #endif /* defined(__pathFindDemo__TileMapScene__) */
